659560ab6ef1f35a699db29336971cf114562548
KorobskoyRoman <shopone023@gmail.com>
2022-02-28 17:41:09 +0300

    fixed alert

--- UnsplashImages/Extensions/Alert.swift
+++ UnsplashImages/Extensions/Alert.swift
@@ -34,24 +34,28 @@ class Alert {
         alertView.clipsToBounds = true
         alertView.layer.cornerRadius = 10
         alertView.addSubview(backgroundImage)
+        alertView.layer.borderWidth = 5
+        alertView.layer.borderColor = UIColor.mainWhite().cgColor
         
         let titleLabel = UILabel(frame: CGRect(x: 0, y: 5, width: alertView.frame.width, height: 44))
         titleLabel.text = title
         titleLabel.font = UIFont.boldSystemFont(ofSize: 30)
         titleLabel.textColor = .systemRed
         titleLabel.textAlignment = .center
+        titleLabel.font = UIFont(name: "American Typewriter Bold", size: 30)
         alertView.addSubview(titleLabel)
         
-        let messageLabel = UILabel(frame: CGRect(x: 80, y: 5, width: alertView.frame.width - 160, height: alertView.frame.height - 10))
+        let messageLabel = UILabel(frame: CGRect(x: 30, y: 0, width: alertView.frame.width - 60, height: alertView.frame.height - 10))
         messageLabel.text = message
         messageLabel.textAlignment = .center
         messageLabel.numberOfLines = 0
+        messageLabel.font = UIFont(name: "American Typewriter Bold", size: 16)
         alertView.addSubview(messageLabel)
         
         let okAction = UIButton(frame: CGRect(x: 140, y: alertView.frame.height - 50, width: alertView.frame.width - 280, height: 40))
         okAction.applyGradients(cornerRadius: 10)
         okAction.titleLabel?.font = UIFont(name: "American Typewriter Bold", size: 20)
-        okAction.titleLabel?.text = "Got it!"
+        okAction.setTitle("Got it!", for: .normal)
         okAction.titleLabel?.textColor = .mainWhite()
         okAction.addTarget(self, action: #selector(dismissAlert), for: .touchUpInside)
         alertView.addSubview(okAction)
@@ -62,7 +66,6 @@ class Alert {
     }
     
     @objc func dismissAlert() {
-        
         guard let targetView = myTargetView else { return }
         UIView.animate(withDuration: 0.3) {
             self.alertView.frame = CGRect(x: 40, y: targetView.frame.height, width: targetView.frame.width - 80, height: 300)
--- UnsplashImages/Extensions/UIView + Extension.swift
+++ UnsplashImages/Extensions/UIView + Extension.swift
@@ -12,7 +12,7 @@ extension UIView {
     func applyGradients(cornerRadius: CGFloat) {
         self.backgroundColor = nil
         self.layoutIfNeeded()
-        let gradientView = GradientView(from: .topTrailing, to: .bottomLeading, startColor: #colorLiteral(red: 1, green: 0.1719351113, blue: 0.4505646229, alpha: 1), endColor: #colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1))
+        let gradientView = GradientView(from: .leading, to: .trailing, startColor: #colorLiteral(red: 1, green: 0.1719351113, blue: 0.4505646229, alpha: 1), endColor: #colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1))
         if let gradientLayer = gradientView.layer.sublayers?.first as? CAGradientLayer {
             gradientLayer.frame = self.bounds
             gradientLayer.cornerRadius = cornerRadius
